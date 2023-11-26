7497125eedb2e333aff36da8d2554c1653427f06
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-11-26 21:39:11 +0100

    changed serializer in producerConfig

--- src/main/java/com/MP3/orderservice/config/KafkaProducerConfig.java
+++ src/main/java/com/MP3/orderservice/config/KafkaProducerConfig.java
@@ -1,8 +1,7 @@
 package com.MP3.orderservice.config;
 
 import com.MP3.orderservice.event.OrderPlacedEvent;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +26,7 @@ public class KafkaProducerConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(JsonDeserializer.TYPE_MAPPINGS,
+        props.put(JsonSerializer.TYPE_MAPPINGS,
                 "orderPlacedEvent:com.MP3.orderService.event.OrderPlacedEvent, orderLineDto:com.MP3.orderService.dto.OrderLineDto");
 
         return props;
