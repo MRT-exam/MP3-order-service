0f0060214a9e3dbdf8026841f3c2933e3748f0d6
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-11-26 15:48:34 +0100

    fixed minor error

--- src/main/java/com/MP3/orderservice/config/KafkaProducerConfig.java
+++ src/main/java/com/MP3/orderservice/config/KafkaProducerConfig.java
@@ -32,12 +32,12 @@ public class KafkaProducerConfig {
         return props;
     }
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfig());
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
