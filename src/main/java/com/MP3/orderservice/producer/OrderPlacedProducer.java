fd15209e3a00338c5e2fa106e4dd7d7b1dc6040f
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-11-26 11:44:40 +0100

    minor changes

--- src/main/java/com/MP3/orderservice/producer/OrderPlacedProducer.java
+++ src/main/java/com/MP3/orderservice/producer/OrderPlacedProducer.java
@@ -1,14 +1,14 @@
 package com.MP3.orderservice.producer;
 
 import com.MP3.orderservice.event.OrderPlacedEvent;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class OrderPlacedProducer {
 
+    @Autowired
     private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void produce(OrderPlacedEvent event) {
